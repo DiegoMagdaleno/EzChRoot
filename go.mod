fe4b60f97cabdf882e68a68b223477c00d6d9d30
Diego Magdaleno <diegomagdaleno@protonmail.com>
2020-02-26 20:13:03 -0600

    Full otool pipe implementation
    - Full otool pipe implementation,
      gets unique elements from a final
      array of selected libs
    
    - Start mapping the strucuture for the multiple
      commands

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/diegomagdaleno/EzChRoot
+
+go 1.13
+
+require (
+	github.com/b4b4r07/go-pipe v0.0.0-20191010045404-84b446f57366
+	github.com/mitchellh/go-homedir v1.1.0
+	github.com/spf13/cobra v0.0.6
+	github.com/spf13/viper v1.6.2
+)
---
+++ lib/otool.go
@@ -0,0 +1,92 @@
+/*
+Copyright © 2020 Diego Magdaleno <diegomagdaleno@protonmail.com>
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice,
+   this list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its contributors
+   may be used to endorse or promote products derived from this software
+   without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+POSSIBILITY OF SUCH DAMAGE.
+*/
+
+package lib
+
+import (
+	"io/ioutil"
+	"os/exec"
+	"regexp"
+	"strings"
+)
+
+func stringInSlice(str string, list []string) bool {
+	for _, v := range list {
+		if v == str {
+			return true
+		}
+	}
+	return false
+}
+
+/*GetLinkedLibs exports unique list of libs required for the programs
+to run under the chroot. Logic can be improved so
+TODO: Improve piping method*/
+func GetLinkedLibs(libFile string) ([]string, error) {
+	regex, err := regexp.Compile("\n\n")
+	cleaned := []string{}
+	content, err := ioutil.ReadFile(libFile)
+	var (
+		libs      []string
+		firstCalc []string
+		libsClean []string
+		splitOut  []string
+	)
+	if err != nil {
+		panic("What?")
+	}
+	lines := strings.Split(string(content), "\n")
+	for i := range lines {
+		cmd := "otool -L " + lines[i] + "| sed 1d | awk '{print $1}' | xargs echo -n"
+		out, err := exec.Command("bash", "-c", cmd).Output()
+		if err != nil {
+			panic("Huoh")
+		}
+		splitOut = strings.Split((string(out)), " ")
+		for k := range splitOut {
+			firstCalc = append(firstCalc, splitOut[k])
+		}
+	}
+	for i := range firstCalc {
+		var secondCalc string
+		secondCalc = regex.ReplaceAllString(firstCalc[i], "\n")
+		libs = append(libs, secondCalc)
+		final := strings.TrimSpace(libs[i])
+		libsClean = append(libsClean, final)
+	}
+	for _, value := range libsClean {
+
+		if !stringInSlice(value, cleaned) {
+			cleaned = append(cleaned, value)
+		}
+	}
+	return cleaned, err
+}
