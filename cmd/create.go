b01ac3d048245e60b3f37be48c0a5e8a403e3e0f
Diego Magdaleno <diegomagdaleno@protonmail.com>
2020-06-14 19:16:41 -0500

    First time of writting into JSON
    Still needs to append rather than
    writing

--- cmd/create.go
+++ cmd/create.go
@@ -85,6 +85,7 @@ to quickly create a Cobra application.`,
 		s.FinalMSG = "\342\234\223 Done calculating dependencies... \n"
 		s.Start()
 		coreLibs = lib.GetLinkedLibs(coreApplications)
+
 		s.Stop()
 
 		s.Suffix = " Copying core system into selected directory... "
@@ -101,7 +102,7 @@ to quickly create a Cobra application.`,
 		lib.CopyAdditionalSettings(path + "/" + name)
 		s.Stop()
 
-		lib.WriteNewPath(name, path)
+		lib.WriteNewPath(name, path, coreApplications, coreLibs)
 	},
 }
 
